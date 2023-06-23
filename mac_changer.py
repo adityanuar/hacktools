75fb5ab24587a4b3133b62d2f0c00506033c7335
adit <adityanuar@gmail.com>
2023-06-23 19:50:09 -0400

    update

--- mac_changer.py
+++ mac_changer.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-subprocess.call("ifconfig", shell=True)
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its Mac address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
