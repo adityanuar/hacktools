cc3ecc53356457c45e63ecda90e93a14aadfe61d
adit <adityanuar@gmail.com>
2023-06-24 03:24:37 -0400

    validation

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -20,10 +21,18 @@ def get_arguments():
         parser.error("[-] Please specify a new mac, use --help for more info.")
     return options
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
 options = get_arguments()
 change_mac(options.interface, options.new_mac)
-
-
-
-
-
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed.")
