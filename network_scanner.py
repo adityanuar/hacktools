54b677b31650ed33925b146a4e54cd8ab9ea25cd
adit <adityanuar@gmail.com>
2023-06-24 20:26:33 -0400

    network scan complete

--- network_scanner.py
+++ network_scanner.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--range", dest="range", help="Range of IPs to discover")
+    options = parser.parse_args()
+    if not options.range:
+        parser.error("[-] Please specify a range, use --help for more info.")
+    return options
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-scan("192.168.19.1/24")
+    print("IP\t\t\tMac Address")
+    print("-------------------------------------------------------")
+    for element in answered_list:
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+
+options = get_arguments()
+scan(options.range)
